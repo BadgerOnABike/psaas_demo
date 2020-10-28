086cf2eee1c843787a9b556830fdb01bcd304e6f
Spydmobile <spydmobile@gmail.com>
2020-10-28 09:47:21 -0600

    typo

--- updateApi.sh
+++ updateApi.sh
@@ -1,2 +1,2 @@
-npm uninstall psaas-js-api && npm install https://spydmobile@bitbucket.org/psaasredapp/psaas-js-api.git -- save
+npm uninstall psaas-js-api && npm install https://spydmobile@bitbucket.org/psaasredapp/psaas-js-api.git --save
 
